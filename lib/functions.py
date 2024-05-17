8cf0255232a4a3b01665a911341103d3d07332e5
skmutai6 <skmutai6@gmail.com>
2024-05-17 15:26:18 +0300

    functions

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print(f"Hello, programmer!")
+greet_programmer()
 
-def greet(name):
-    pass
+
+def greet(name = "Friend"):
+    print(f"Hello, {name}!")
+greet("Friends")
+
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+greet_with_default()
+
 
 def add(num1, num2):
-    pass
+    print(num1 + num2)
+add(1, 2)
 
 def halve(number):
-    pass
+    print(number/2)
+halve(2)
